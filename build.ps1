0192d3302f8454c8cbaf4c837dd4cbbbf03b16ad
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-07-07 17:30:09 +0200

    cleaned up scripts

--- build.ps1
+++ build.ps1
@@ -1,5 +1,2 @@
-# Pyproj Builder for Windows Systems
-
 pyinstaller --noconfirm --onefile --console --name "pyproj.exe" "src/main.py"
-
 echo "Building of Pyproj executable DONE!"
--- install.ps1
+++ install.ps1
@@ -1,5 +1,3 @@
-# Pyproj Installer for Windows Systems
-
 New-Item "C:/Users/$Env:USERNAME/AppData/Local/pyproj" -ItemType "directory"
 xcopy.exe "dist" "C:/Users/$Env:USERNAME/AppData/Local/pyproj"
 [Environment]::SetEnvironmentVariable("Path", $Env:PATH + ";C:/Users/$Env:USERNAME/AppData/Local/pyproj;", "User")
