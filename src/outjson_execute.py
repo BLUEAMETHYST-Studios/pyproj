ecd237a83b37146d7e75819185677e9c842908da
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-07-10 13:07:31 +0200

    Update outjson_execute.py
    fixed logging, so that the terminal looks good

--- src/outjson_execute.py
+++ src/outjson_execute.py
@@ -1,10 +1,10 @@
-from subprocess import check_output
+from subprocess import run, DEVNULL, STDOUT
 from colorama import Fore
 from sys import exit as kill
 
 def outjson_exec(args):
     try:
-        check_output(args) # Using check_output, so that logging is invisible
+        run(args, stdout=DEVNULL, stderr=STDOUT)
     except FileNotFoundError:
         print(f"[❌] {Fore.RED}Was not able to build, due to PyInstaller not being installed!")
         kill()
